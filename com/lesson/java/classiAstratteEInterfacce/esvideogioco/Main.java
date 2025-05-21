8199a5c7c05907d9e293675a35df63ff3d577312
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-05-21 12:16:30 +0200

    add: Videogioco V2 con input utente

--- com/lesson/java/classiAstratteEInterfacce/esvideogioco/Main.java
+++ com/lesson/java/classiAstratteEInterfacce/esvideogioco/Main.java
@@ -1,13 +1,23 @@
 package esvideogioco;
 
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
     CurrentGame partita = new CurrentGame();
     GestioneVideogioco.start(partita);
-    partita.gioco();
-    partita.gioco();
-    partita.gioco();
+
+    Scanner scanner = new Scanner(System.in);
+    String input;
+
+    do {
+            partita.gioco();
+            System.out.print("Vuoi continuare a giocare? (n per terminare): ");
+            input = scanner.nextLine();
+        } while (!input.equalsIgnoreCase("n"));
+
     GestioneVideogioco.gameOver(partita);
+    scanner.close();
     }
 }
