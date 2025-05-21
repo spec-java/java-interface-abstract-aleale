62623c011d1ff9caac05b483076115b34715bd83
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-05-21 12:03:38 +0200

    NON COMPLETO add: videogioco

---
+++ com/lesson/java/classiAstratteEInterfacce/esvideogioco/CurrentGame.java
@@ -0,0 +1,24 @@
+package esvideogioco;
+
+public class CurrentGame {
+    
+    private boolean isAlive;
+
+    public void gioco(){
+        
+    }
+
+    public boolean isIsAlive() {
+        return this.isAlive;
+    }
+
+    public boolean getIsAlive() {
+        return this.isAlive;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+
+}
---
+++ com/lesson/java/classiAstratteEInterfacce/esvideogioco/GestioneVideogioco.java
@@ -0,0 +1,12 @@
+package esvideogioco;
+
+public interface GestioneVideogioco {
+    
+    static void start(){
+        System.out.println("La partita inizia");
+    }
+
+    static void gameOver(){
+        System.out.println("La partita è finita");
+    }
+}
